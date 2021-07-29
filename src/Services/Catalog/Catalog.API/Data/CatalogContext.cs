1ec98035b830b5dc98e30d7cce9bce8b1f714430
carlosr1110 <carlosrb1110@gmail.com>
2021-07-29 13:31:41 -0500

    CatalogContext

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            var client = new MongoClient(configuration.GetConnectionString("DefaultConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("ConnectionStrings:DatabaseName"));
+            var Products = database.GetCollection<Product>(configuration.GetValue<string>("ConnectionStrings:CollectionName"));
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
